a4b232712a10f2ac4033391eb0f34bd239154b81
bright-crucible <bright-crucible@users.noreply.github.com>
2020-02-22 20:31:11 -0800

    Added script to rename extensionless mkvs

---
+++ sabnzbd/rename_mkv.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+USAGE="$0"
+FINAL_DIR="$1"
+JOB_NAME="$2"
+INDEX_REPORT="$3"
+CATEGORY="$5"
+GROUP="$6"
+STATUS="$7"
+URL="$8"
+
+MATROSKA="Matroska data"
+EXT="mkv"
+EXIT_CODE=0
+
+if [ -z $FINAL_DIR ]
+then
+    echo "Missing Final Directory..." >&2
+    exit 1
+fi
+
+echo "Job name: ${JOB_NAME}"
+echo "Final directory: ${FINAL_DIR}"
+echo "Index report: ${INDEX_REPORT}"
+echo "Category: ${CATEGORY}"
+echo "Processing status: ${STATUS}"
+echo "Group: ${GROUP}"
+echo "URL: ${URL}"
+
+for entry in "$FINAL_DIR"/*
+do
+    TYPE=`file -b "${entry}"`
+    if [ "${TYPE}" == "${MATROSKA}" ]
+    then
+        echo "$entry"
+        BASENAME=`basename "${entry}"`
+        EXTENSION="${BASENAME##*.}"
+        echo $EXTENSION
+        if [ "${EXTENSION}" != "${EXT}" ]
+        then
+            if mv "${entry}" "${entry}.${EXT}"
+            then
+                echo "Renamed ${entry} to ${entry}.${EXT}"
+            else
+                echo "Failed, could not rename ${entry} to ${entry}.${EXT}" >&2
+                EXIT_CODE=1
+            fi
+        fi
+    fi
+done
+exit ${EXIT_CODE}
